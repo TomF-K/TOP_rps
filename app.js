cacee332deb27105b7fd63dcb3788839b726b833
Tom Forman-Kenrick <tom.kenrick@gmail.com>
2023-10-08 18:18:47 +0100

    setup up random computer choice function

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 // select elements for player selection
 
-// set up an array for computerChoice and randomly return a value 
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
 
+// set up an array for computerChoice  
+
+const computerChoice = ['rock', 'paper', 'scissors'];
+
+// create a function that randomly returns the computer choice
+
+let randomNumber = Math.floor(Math.random() * computerChoice.length);
+
+console.log(randomNumber);
+
+function getComputerChoice() {
+    return computerChoice[randomNumber]};
+
+console.log(getComputerChoice());
